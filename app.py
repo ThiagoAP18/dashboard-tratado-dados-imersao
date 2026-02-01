945fe24240d83b048a0f3625d370773c62e003f7
ThiagoAP18 <thiagopedrosa71@gmail.com>
2026-02-01 12:35:08 -0300

    Alterando caminho do .csv

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ st.set_page_config(
 )
 
 #Carregamento de Planilha
-df = pd.read_csv("df-imersao-dados.csv")
+df = pd.read_csv("..\df-imersao-dados.csv")
 
 #Barra Lateral
 st.sidebar.header("🔍 Filtros")
